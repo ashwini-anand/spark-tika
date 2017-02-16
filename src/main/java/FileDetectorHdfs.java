8d3ec07e5837f454177f98c262b9885aecef5637
Ashwini Anand <asanand@HW12686.local>
2017-02-16 18:48:30 +0530

    commiting FileDetectorHdfs.java, this class takes 2 arguments inputpath and outputpath.

--- src/main/java/FileDetectorHdfs.java
+++ src/main/java/FileDetectorHdfs.java
@@ -1,4 +1,7 @@
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.input.PortableDataStream;
@@ -8,20 +11,23 @@ import scala.Tuple2;
 
 import java.io.DataInputStream;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
  * Created by asanand on 2/14/17.
  */
-public class FileDetector {
+public class FileDetectorHdfs {
     public static void main(String[] args) {
         JavaSparkContext sc = new JavaSparkContext();
-        JavaPairRDD<String, PortableDataStream> files = sc.binaryFiles("/Users/asanand/Downloads/sparkTikaTest/sparkTest3");
-        //JavaPairRDD<String, PortableDataStream> files = sc.binaryFiles("hdfs:/user/admin2");
+        JavaPairRDD<String, PortableDataStream> files = sc.binaryFiles(args[0]); //like "hdfs:/user/admin2"
         try {
-            List<Map<String, String>> results = files.map(new Function<Tuple2<String, PortableDataStream>, Map<String, String>>() {
+            FileSystem fs = FileSystem.get(sc.hadoopConfiguration());
+            Path outputPath = new Path(args[1]);
+            if (fs.exists(outputPath)) {
+                fs.delete(outputPath, true);
+            }
 
+            JavaRDD<Map<String, String>> results = files.map(new Function<Tuple2<String, PortableDataStream>, Map<String, String>>() {
                 public Map<String, String> call(Tuple2<String, PortableDataStream> stringPortableDataStreamTuple2) throws Exception {
                     DataInputStream dis = stringPortableDataStreamTuple2._2.open();
                     Tika tika = new Tika();
@@ -29,25 +35,17 @@ public class FileDetector {
                     Metadata tikaMetadata = new Metadata();
                     tika.parse(dis, tikaMetadata);
                     String[] names = tikaMetadata.names();
-                    metadata.put("File Name", stringPortableDataStreamTuple2._1);
+                    metadata.put("File Name", stringPortableDataStreamTuple2._1 + "\n");
                     for (String name : names) {
-                        metadata.put(name, tikaMetadata.get(name));
+                        metadata.put(name, tikaMetadata.get(name) + "\n");
                     }
+                    metadata.put("----------------------------END------------------------", "\n\n");
                     return metadata;
                 }
-            }).collect();
-            System.out.println("\n\n");
-
-            for (Map<String, String> fileMetadata : results
-                    ) {
-                for (String key : fileMetadata.keySet()
-                        ) {
-                    System.out.println(key + " : " + fileMetadata.get(key));
-                }
-                System.out.println();
-            }
+            });
+            results.saveAsTextFile(args[1]); // is coalesce(1,true).saveAsTextFile needed ?
         } catch (Exception e) {
-            System.err.println("exception in FileDetector");
+            System.err.println("exception in FileDetectorHdfs");
             e.printStackTrace();
         }
 
